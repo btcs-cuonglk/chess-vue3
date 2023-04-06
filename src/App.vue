abad976a910b857c5a30d8dca684420884a9f025
Cuonglk <cuonglk@btc-studio.io>
2023-04-06 08:44:13 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+import TodoList from './views/TodoList/index.vue'
+</script>
+
+<template>
+  <header>
+    <h1 class="text-center font-bold text-3xl my-5 text-white">Chess</h1>
+  </header>
+
+  <TodoList class="text-red-700"></TodoList>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+header {
+}
+</style>
